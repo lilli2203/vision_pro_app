cc776c322577acc25a1488dbda3b9813b92263ff
lilli2203 <mailyourlilli@gmail.com>
2024-07-23 14:47:05 +0530

    more immerise

--- ImmersiveView.swift
+++ ImmersiveView.swift
@@ -1,80 +1,108 @@
-
 import SwiftUI
 import RealityKit
 import RealityKitContent
+import Combine
 
 struct ImmersiveView: View {
     @Environment(ViewModel.self) private var model
     @State private var planeEntity: Entity?
+    @State private var isLoaded = false
     
     var body: some View {
         @Bindable var model = model
         RealityView { content in
             if let planeEntity = planeEntity {
-                            content.add(planeEntity)
-                        }
-                    }
-                    .onAppear {
-                        updatePlaneEntity() // Initial setup
-                    }
-                    .onChange(of: model.flowState) { _,_ in
-                        updatePlaneEntity() 
-                        print("updating img")// Update when flowState changes
-                    }
-        
-        
+                content.add(planeEntity)
+            }
         }
-       
+        .onAppear {
+            updatePlaneEntity() // Initial setup
+        }
+        .onChange(of: model.flowState) { _,_ in
+            updatePlaneEntity()
+            print("Flow state changed, updating plane entity")
+        }
+    }
     
-    private func updatePlaneEntity(){
+    private func updatePlaneEntity() {
         let wallAnchor = AnchorEntity(.plane(.vertical, classification: .wall, minimumBounds: SIMD2<Float>(1.0, 0.2)))
         
         let planeMesh = MeshResource.generatePlane(width: 0.70, depth: 4.775, cornerRadius: 0.0)
         
-        let img = Image("panel.png")
-            .resizable(resizingMode: .tile)
-        
         let material: SimpleMaterial
-                switch model.flowState {
-                case .one:
-                    material = ImmersiveView.loadImageMaterial(imageUrl: "panel.png")
-                case .two:
-                    material = ImmersiveView.loadImageMaterial(imageUrl: "wally.png")
-                case .three:
-                    material = ImmersiveView.loadImageMaterial(imageUrl: "panel.png")
-                }
-    
+        switch model.flowState {
+        case .one:
+            material = ImmersiveView.loadImageMaterial(imageUrl: "panel.png")
+        case .two:
+            material = ImmersiveView.loadImageMaterial(imageUrl: "wally.png")
+        case .three:
+            material = ImmersiveView.loadImageMaterial(imageUrl: "panel.png")
+        }
         
         let planeEntity = ModelEntity(mesh: planeMesh, materials: [material])
-        
-           planeEntity.name = "canvas"
-           wallAnchor.addChild(planeEntity)
+        planeEntity.name = "canvas"
+        wallAnchor.addChild(planeEntity)
         
         self.planeEntity = wallAnchor
-       }
-    
-
-    
+        isLoaded = true
+    }
     
     static func loadImageMaterial(imageUrl: String) -> SimpleMaterial {
-            do {
-                let texture = try TextureResource.load(named: imageUrl)
-                
-            
-                
-                var material = SimpleMaterial()
-                material.baseColor = MaterialColorParameter.texture(texture)
-                
-                
-                return material
-            } catch {
-                fatalError(String(describing: error))
-            }
+        do {
+            let texture = try TextureResource.load(named: imageUrl)
+            var material = SimpleMaterial()
+            material.baseColor = MaterialColorParameter.texture(texture)
+            return material
+        } catch {
+            fatalError(String(describing: error))
         }
     }
-//UNABle to see pictures after one full scroll
+}
 
 #Preview {
     ImmersiveView()
         .previewLayout(.sizeThatFits)
+}
+
+// ViewModel to manage flow state
+class ViewModel: ObservableObject {
+    @Published var flowState: FlowState = .one
+    
+    enum FlowState {
+        case one, two, three
+    }
+}
+
+// Additional Views to demonstrate state changes
+struct ContentView: View {
+    @StateObject private var viewModel = ViewModel()
+    
+    var body: some View {
+        VStack {
+            ImmersiveView()
+                .environment(viewModel)
+            
+            Picker("Flow State", selection: $viewModel.flowState) {
+                Text("One").tag(ViewModel.FlowState.one)
+                Text("Two").tag(ViewModel.FlowState.two)
+                Text("Three").tag(ViewModel.FlowState.three)
+            }
+            .pickerStyle(SegmentedPickerStyle())
+            .padding()
+            
+            Button("Reset Flow State") {
+                viewModel.flowState = .one
+            }
+            .padding()
+        }
+    }
+}
+
+@main
+struct MyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
 }
